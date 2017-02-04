0eeee47780dc47dd102a80c280b491a7ef6a3651
Robin Karlsson <robinpkarlsson@gmail.com>
2017-02-04 10:28:04 +0100

    Rename convertHours/Minutes helpers

--- utils/unformat-time.js
+++ utils/unformat-time.js
@@ -1,13 +1,14 @@
-const calcHours = n => n ? n * 60 * 60 : 0
-const calcMinutes = n => n *  60
-
 /**
  * Parses a timestamp string (hh:mm:ss or mm:ss)
  * and returns the total amount of seconds
 */
+
+const hoursToSeconds = n => n ? n * 60 * 60 : 0
+const minutesToSeconds = n => n *  60
+
 module.exports = string => {
   const [seconds, minutes, hours] = string.split(':')
         .reverse()
         .map(x => x ? parseInt(x) : x)
-  return calcHours(hours) + calcMinutes(minutes) + seconds
+  return hoursToSeconds(hours) + minutesToSeconds(minutes) + seconds
 };
