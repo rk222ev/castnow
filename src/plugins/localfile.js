6b17c09486bb1cd6bf67dbdcf197284d46c10529
Robin Karlsson <robinpkarlsson@gmail.com>
2017-02-08 18:47:40 +0100

    Remove router dependency and remove ; from localfile

--- src/plugins/localfile.js
+++ src/plugins/localfile.js
@@ -1,33 +1,31 @@
-var http = require('http');
-var internalIp = require('internal-ip');
-var router = require('router');
-var path = require('path');
-var serveMp4 = require('../utils/serve-mp4');
-var debug = require('debug')('castnow:localfile');
-var fs = require('fs');
+var http = require('http')
+var internalIp = require('internal-ip')
+var path = require('path')
+var serveMp4 = require('../utils/serve-mp4')
+var debug = require('debug')('castnow:localfile')
+var fs = require('fs')
 
 var isFile = function(item) {
-  return fs.existsSync(item.path) && fs.statSync(item.path).isFile();
-};
+  return fs.existsSync(item.path) && fs.statSync(item.path).isFile()
+}
 
 var contains = function(arr, cb) {
   for (var i=0, len=arr.length; i<len; i++) {
-    if (cb(arr[i], i)) return true;
+    if (cb(arr[i], i)) return true
   }
-  return false;
-};
+  return false
+}
 
 var localfile = function(ctx, next) {
-  if (ctx.mode !== 'launch') return next();
-  if (!contains(ctx.options.playlist, isFile)) return next();
+  if (ctx.mode !== 'launch') return next()
+  if (!contains(ctx.options.playlist, isFile)) return next()
 
-  var route = router();
-  var list = ctx.options.playlist.slice(0);
-  var ip = (ctx.options.myip || internalIp());
-  var port = ctx.options['localfile-port'] || 4100;
+  var list = ctx.options.playlist.slice(0)
+  var ip = (ctx.options.myip || internalIp())
+  var port = ctx.options['localfile-port'] || 4100
 
   ctx.options.playlist = list.map(function(item, idx) {
-    if (!isFile(item)) return item;
+    if (!isFile(item)) return item
     return {
       path: 'http://' + ip + ':' + port + '/' + idx,
       type: 'video/mp4',
@@ -37,22 +35,27 @@ var localfile = function(ctx, next) {
           title: path.basename(item.path)
         }
       }
-    };
-  });
-
-  route.all('/{idx}', function(req, res) {
-    if (!list[req.params.idx]) {
-      res.statusCode = '404';
-      return res.end('page not found');
     }
-    debug('incoming request serving %s', list[req.params.idx].path);
-    serveMp4(req, res, list[req.params.idx].path);
-  });
+  })
 
-  http.createServer(route).listen(port);
-  debug('started webserver on address %s using port %s', ip, port);
-  next();
+  const requestListener = (req, res) => {
+    const idx = req.url.slice(1)
 
-};
+    if (!list[idx]) {
+      res.statusCode = '404'
+      return res.end('page not found')
+    }
 
-module.exports = localfile;
+    debug('incoming request serving %s', list[idx].path)
+    serveMp4(req, res, list[idx].path)
+  }
+
+  http.createServer(requestListener)
+    .listen(port)
+
+  debug('started webserver on address %s using port %s', ip, port)
+  next()
+
+}
+
+module.exports = localfile
